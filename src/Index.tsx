5e8960f851f99af89adfb4d7f0ef30082250eeda
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-09-15 22:16:40 +0430

    Changes added

--- src/Index.tsx
+++ src/Index.tsx
@@ -6,12 +6,12 @@ import App from "./App";
 import ThemeProvider from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
